4211e1b1823a21a11be2286e9e22e7124167dfc1
tessambrown <tebr1887@colorado.edu>
2026-04-14 16:57:28 -0600

    fixed genius headers

--- backend/api/lyrics.py
+++ backend/api/lyrics.py
@@ -14,9 +14,11 @@ from errors import appError
 # set the api key for the lyric genius API
 api_key = os.getenv("LYRIC_KEY")
 genius = lyricsgenius.Genius(api_key)
-genius.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-genius.headers["Accept"] = "application/json"
-genius.headers["Accept-Language"] = "en-US,en;q=0.9"
+genius._session.headers.update({
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept": "application/json",
+    "Accept-Language": "en-US,en;q=0.9"
+})
 genius.timeout = 10
 genius.retries = 1
 
